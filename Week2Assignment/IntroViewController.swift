e733809228980f144ff11900cafc6f7aca12e06d
dscoville <davidscoville@gmail.com>
2015-11-01 23:33:26 -0800

    First commit

---
+++ Week2Assignment/IntroViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IntroViewController.swift
+//  Week2Assignment
+//
+//  Created by David Scoville on 10/28/15.
+//  Copyright © 2015 David Scoville. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = imageView.image!.size
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
